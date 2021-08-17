7512f1b90543c287a0e7f8e39d3ca79999f61130
Swed 911 <swed911@gmail.com>
2021-08-17 23:16:10 +0300

    Add to base

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
+
 
 class ContactController extends Controller
 {
-    public function submit(ContactRequest $req) {
-        /*$validation = $req->validate([
-            'subject' => 'required|min:5|max:50]',
-            'email' => 'required'
-            ]);*/
+    public function submit(ContactRequest $req)
+    {
+        $contact = new Contact();
+        $contact->name = $req->input('name');
+        $contact->email = $req->input('email');
+        $contact->subject = $req->input('subject');
+
+        $contact->save();
+
+        return redirect()->route('home')->with('success', 'Заявка принята' );
+
     }
 }
